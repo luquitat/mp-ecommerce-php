57d5f28519621c8ca6406c9ad353386145a55d94
Lucas <lucas.t.micheli@gmail.com>
2019-09-24 13:44:07 -0300

    dasdada

--- notificaciones.php
+++ notificaciones.php
@@ -1,3 +1,35 @@
 <?php
-print_r($_POST);
+$nombre_archivo = "notificaciones.txt";
+
+if(file_exists($nombre_archivo))
+{
+    $mensaje = "El Archivo $nombre_archivo se ha modificado";
+}
+
+else
+{
+    $mensaje = "El Archivo $nombre_archivo se ha creado";
+}
+
+if($archivo = fopen($nombre_archivo, "a"))
+{
+    if(fwrite($archivo, json_encode($_POST)))
+    {
+        echo "Se ha ejecutado correctamente";
+    }
+    else
+    {
+        echo "Ha habido un problema al crear el archivo";
+    }
+
+    fclose($archivo);
+}
+
+$file = fopen("notificaciones.txt", "r") or exit("Unable to open file!");
+//Output a line of the file until the end is reached
+while(!feof($file))
+{
+    echo fgets($file);
+}
+fclose($file);
 ?>
